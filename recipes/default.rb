3875e4149cd14482b500a8f69c801cba126262ab
Jeffrey Alvarez <Jeffrey_Alvarez@intuit.com>
2021-04-24 12:13:51 -0500

    Forward and mail optional.

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,5 +4,11 @@
 #
 
 include_recipe 'fluentbit::install'
-include_recipe 'fluentbit::forward'
-include_recipe 'fluentbit::mail'
+
+if node['fluentbit']['forward']['enabled']
+  include_recipe 'fluentbit::forward'
+end
+
+if node['fluentbit']['mail']['enabled']
+  include_recipe 'fluentbit::mail'
+end
